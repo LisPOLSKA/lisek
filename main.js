3a8b3eb7d19f13e1fb95d3107f676cc661a4e631
Lisek <minecraft.tajemnice1@gmail.com>
2022-11-08 18:46:11 +0100

    Update file project
    Signed-off-by: Lisek <minecraft.tajemnice1@gmail.com>

---
+++ main.js
@@ -0,0 +1,34 @@
+const switchButton = document.querySelector("header #upa");
+let theme = localStorage.getItem("theme");
+
+switchButton.addEventListener("click", () => {
+    if (theme === "dark") {
+        document.querySelector("body").classList.remove("dark");
+        document.querySelector("body").classList.add("light");
+        document.querySelector("header nav").classList.add("bg-primary");
+        document.querySelector("header nav").classList.remove("bg-dark");
+        theme = "light";
+    } else {
+        document.querySelector("body").classList.remove("light");
+        document.querySelector("body").classList.add("dark");
+        document.querySelector("header nav").classList.add("bg-dark");
+        document.querySelector("header nav").classList.remove("bg-primary");
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
+});
+
+if (theme === "dark") {
+    document.querySelector("body").classList.add("dark");
+    document.querySelector("body").classList.remove("light");
+    document.querySelector("header nav").classList.add("bg-dark");
+    document.querySelector("header nav").classList.remove("bg-primary");
+}
+
+if (theme === "light") {
+    document.querySelector("body").classList.add("light");
+    document.querySelector("body").classList.remove("dark");
+    document.querySelector("header nav").classList.add("bg-primary");
+    document.querySelector("header nav").classList.remove("bg-dark");
+}
